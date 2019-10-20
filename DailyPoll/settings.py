3565d7674d1f0002de0f0bbf7fde8279d53ff851
Taylor Cairns <s3603013@student.rmit.edu.au>
2019-10-20 14:28:57 +1100

    revert database changes

--- DailyPoll/settings.py
+++ DailyPoll/settings.py
@@ -78,20 +78,11 @@ WSGI_APPLICATION = 'DailyPoll.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
-        'default': {
-            'ENGINE': 'sql_server.pyodbc',
-            'NAME': 'theDailyPoll',
-            'USER': 'theDailyPollAdmin',
-            'PASSWORD': 'theDailyPoll?',
-            'HOST': 'thedailypoll.database.windows.net',      
-            'PORT': '1433',
-            'OPTIONS': {
-                'driver': 'ODBC Driver 13 for SQL Server',
-                'dsn': 'theDailyPoll',
-                'MARS_Connection': 'True',
-            }
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 LOGIN_URL = '/login'
 
